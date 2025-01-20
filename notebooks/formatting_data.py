27b3234dfa445719e577b02f7848cddbe58f4c9e
ole <ole.bialas@posteo.de>
2025-01-20 20:47:09 +0100

    get stimuli

--- notebooks/formatting_data.py
+++ notebooks/formatting_data.py
@@ -8,6 +8,7 @@
 
 
 
+
 from pathlib import Path
 from typing import List
 import pandas as pd
@@ -23,6 +24,7 @@ root = Path('/home/obi/projects/Publication-Ready-Figures-With-Matplotlib')
 cache = EcephysProjectCache.from_warehouse(manifest=root/"data"/"raw"/"manifest.json")
 SESSION_TYPE="brain_observatory_1.1"
 ROI = ["VISpm", "VISrl", "VISam"] # primary, rostrolateral and anteriormedial visual cortex
+STIMULI = ["gabors", "flashes", "drift_gratings", "static_gratings", "natural_scenes"]
 GENOTYPES = ["wt/wt"]
 
 
@@ -88,9 +90,19 @@ for session_id in sessions.index:
 
 
 #| ref.label: subject-loop
+    stimuli = session.get_stimulus_table()
+    stimuli = stimuli[stimuli.stimulus_name.isin(STIMULI)]
+
+
+
+
+
+
+#| ref.label: subject-loop
     out_dir = root/"data"/f"ses_{session_id}"
     if not out_dir.exists():
         out_dir.mkdir()
     units.to_csv(out_dir/f"ses_{session_id}_units.csv")
+    stimuli.to_csv(out_dir/f"ses_{session_id}_stimuli.csv")
     np.save(out_dir/f"ses_{session_id}_mean_waveforms.npy", mean_waveforms_out, allow_pickle=True)
     np.save(out_dir/f"ses_{session_id}_spike_times.npy", spike_times_out, allow_pickle=True)
