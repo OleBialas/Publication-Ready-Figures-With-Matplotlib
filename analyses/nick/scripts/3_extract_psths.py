d6dde52fe152e330ca87bd06a7bdabf5451aaba7
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-01-21 12:43:19 +0100

    label cells

--- analyses/nick/scripts/3_extract_psths.py
+++ analyses/nick/scripts/3_extract_psths.py
@@ -13,15 +13,13 @@ from pathlib import Path
 
 from src.utils import compute_psth
 
-# %%
+# %% Select a Sessoin
 session_dirs = list(Path('data/processed').iterdir())
 session_dir = session_dirs[0]
 session_dir
 
-# %%
-print(list(session_dir.iterdir()))
 
-# %%
+# %% Read Data
 stims = pd.read_csv(session_dir / 'stims.csv')
 spikes = np.load(session_dir / 'spike_times.npy', allow_pickle=True).item()
 stims, spikes
