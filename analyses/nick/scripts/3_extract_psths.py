a244f68aea2319aaebdd9eb3dcd16cf24067d0ee
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-01-21 12:43:01 +0100

    get data

--- analyses/nick/scripts/3_extract_psths.py
+++ analyses/nick/scripts/3_extract_psths.py
@@ -1,14 +1,17 @@
+# %%
+
+import os
+os.chdir(Path(__file__).parent.parent)
+os.getcwd()
 
 # %%
 from shutil import copy
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from src.utils import compute_psth
 
-import os
-os.chdir(Path(__file__).parent.parent)
-os.getcwd()
+
+from src.utils import compute_psth
 
 # %%
 session_dirs = list(Path('data/processed').iterdir())
@@ -19,3 +22,7 @@ session_dir
 print(list(session_dir.iterdir()))
 
 # %%
+stims = pd.read_csv(session_dir / 'stims.csv')
+spikes = np.load(session_dir / 'spike_times.npy', allow_pickle=True).item()
+stims, spikes
+
