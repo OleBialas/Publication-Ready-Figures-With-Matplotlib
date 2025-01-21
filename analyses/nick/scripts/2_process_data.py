d70e2e054d81fdb54760a68737c2972fec8aed77
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-01-21 13:46:58 +0100

    units, to parquet

--- analyses/nick/scripts/2_process_data.py
+++ analyses/nick/scripts/2_process_data.py
@@ -17,7 +17,12 @@ Path.cwd()
 # %%
 session_dirs = list(Path('data/raw').iterdir())
 print(session_dirs)
+
+# %%
 for session_dir in session_dirs:
+
+# %%
+    # session_dir = session_dirs[0]
     session_dir
 
     # %%
@@ -32,18 +37,21 @@ for session_dir in session_dirs:
     # %% Get Stimuli
     stims = pd.read_csv(list(session_dir.glob('*_stimuli.csv'))[0])
     gabor_stims = stims[stims['stimulus_name'] == 'gabors'][['start_time', 'x_position', 'y_position', 'orientation']]
-    gabor_stims.to_csv(processed_dir / 'stims.csv', index=False)
+    gabor_stims.to_parquet(processed_dir / 'stims.parquet')
     gabor_stims
 
-
     # %%  Load Spike Times
-    spike_times_path = list(session_dir.glob('*_spike_times.npy'))[0]
-    spike_times = np.load(spike_times_path, allow_pickle=True).item()
-    spike_times
-    print(spike_times.keys())
-    list(spike_times.values())[0].shape
-    copy(spike_times_path, str(processed_dir / 'spike_times.npy'))
-    spike_times
+    
+    spike_times = np.load(list(session_dir.glob('*_spike_times.npy'))[0], allow_pickle=True).item()
+    units = pd.read_csv(list(session_dir.glob('*_units.csv'))[0]).set_index('unit_id')
+    
+    st = pd.Series(data=list(spike_times.values()), index=list(spike_times.keys()), name='spike_times')
+    
+    units2 = units.copy()
+    units2['spike_times'] = st
+    units2.to_parquet(processed_dir / 'units.parquet')
+
+
 
 
 
