a5675f8612e3c1a895c9ef5209c1dd14dfc98dc5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-01-21 12:00:45 +0100

    nice download

--- analyses/nick/scripts/1_download_data.py
+++ analyses/nick/scripts/1_download_data.py
@@ -1,4 +1,5 @@
 import os
+from shutil import copytree
 import sys
 from pathlib import Path
 import requests
@@ -7,26 +8,31 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+copytree
 
 
-
+root = Path.cwd()
 
 print('downloading data...')
-cache_dir = Path(os.getcwd()) / 'cache/data.zip'
-cache_dir.parent.mkdir(exist_ok=True, parents=True)
-if not cache_dir.exists():
-        response = requests.get("https://uni-bonn.sciebo.de/index.php/s/bIDNCBs37frcli8/download", stream=True)
-        response.raise_for_status()
-        with cache_dir.open(mode='wb') as f:
-            for chunk in tqdm(response.iter_content(chunk_size=8192)):
-                if chunk:
-                    f.write(chunk)
+cache_dir = root / 'cache'
+cache_dir.mkdir(exist_ok=True, parents=True)
+cache_archive = cache_dir / 'data.zip'
+if not cache_archive.exists():
+    response = requests.get("https://uni-bonn.sciebo.de/index.php/s/bIDNCBs37frcli8/download", stream=True)
+    response.raise_for_status()
+    with cache_archive.open(mode='wb') as f:
+        for chunk in tqdm(response.iter_content(chunk_size=8192)):
+            if chunk:
+                f.write(chunk)
+
+print('extracting data...')
+expected_folder = cache_dir / 'visual_coding_spiking_data'
+if not expected_folder.exists():
+    with zipfile.ZipFile(str(cache_archive), 'r') as zip_ref:
+        zip_ref.extractall(cache_dir)
+if not expected_folder.exists():
+    raise FileNotFoundError("Folder wasn't created: " + str(expected_folder))
 
 
-
-
-root = Path(os.getcwd()) / 'data/raw'
-root.mkdir(exist_ok=True, parents=True)
-if not list(root.iterdir()):
-    with zipfile.ZipFile(str(cache_dir), 'r') as zip_ref:
-        zip_ref.extractall(root)
+print('copying to data/raw...')
+copytree(expected_folder, 'data/raw')
