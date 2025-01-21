9a90fce745a18a33ada75736652dc47aeb656c85
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-01-21 12:54:37 +0100

    units

--- analyses/nick/scripts/3_extract_psths.py
+++ analyses/nick/scripts/3_extract_psths.py
@@ -29,9 +29,31 @@ stims, spikes
 units = spikes[915957872]
 
 # %%
-start_time = stims['start_time']
-start_time
-
-
-bin_centers, counts = compute_psth(spike_times=units, stim_times=start_time, bin_width=.01)
+bin_centers, counts = compute_psth(spike_times=units, stim_times=stims['start_time'], bin_width=.01)
 plt.plot(bin_centers, counts)
+
+
+# %%
+stims
+
+# %%
+
+spikes_rel = units - stims['start_time'][1]
+trial_spikes = spikes_rel[(-0.5 <= spikes_rel) & (spikes_rel < 0.7)]
+trial_spikes
+
+
+# %% Get all
+trials = {}
+for _, stim in stims.iterrows():
+    spikes_rel = units - stim['start_time']
+    trial_spikes = spikes_rel[(-0.5 <= spikes_rel) & (spikes_rel < 0.7)]
+    trial_spikes = trial_spikes.tolist()
+    trial = {
+        'start_time': stim.start_time,
+        'x': stim.x_position,
+        'y': stim.y_position,
+        'ori': stim.orientation,
+        'spikes': trial_spikes
+    }
+
